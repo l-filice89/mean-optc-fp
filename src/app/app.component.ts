4751e3255bb6a01cf918e20838be939f09b7169c
l-filice <l.filice@studenti.unibg.it>
2018-12-30 12:42:04 +0100

    Basic page setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'mean-optc-fp';
+  title = 'OPTC-FP';
+
+  public constructor(private titleService: Title){}
+
+  public setTitle(newTitle: string){
+    this.titleService.setTitle(newTitle);
+  }
 }
